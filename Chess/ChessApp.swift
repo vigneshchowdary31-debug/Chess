b0b59aa4b28a67cf899236317d5e655ebd0cb3f9
vigneshchowdary31-debug <vigneshchowdary31@gmail.com>
2025-11-24 21:35:59 +0530

    Initial Commit

---
+++ Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Vignesh Chowdary on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chess/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chess
+//
+//  Created by Vignesh Chowdary on 24/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
